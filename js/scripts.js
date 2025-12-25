39eb1db0e0eb62c1852d2ff6e1db0e72a2e9e77d
Richard Lin <richlin3452@gmail.com>
2025-12-24 22:03:00 -0500

    test navbar with js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+fetch("../nav.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  })
+  .catch(error => console.log("Error loading navbar:", error));
