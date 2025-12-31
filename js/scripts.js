e6de6b0a7baf333a6189c6ad9db992c3dc87547e
Richard Lin <richlin3452@gmail.com>
2025-12-30 19:49:57 -0500

    add navbar

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ fetch("/Richard-Lin/nav.html")
     document.getElementById("navbar").innerHTML = data;
   })
   .catch(error => console.log("Error loading navbar:", error));
+
+fetch("/Richard-Lin/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  })
+  .catch(error => console.log("Error loading footer:", error));
